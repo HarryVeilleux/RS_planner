530fbacc437038a5dbc7ae04cec813515d6694a3
HarryVeilleux <35110097+HarryVeilleux@users.noreply.github.com>
2018-08-13 12:16:08 -0400

    Added pandas functionality, added skills.csv
    calc_functions.py now contains acts_lvl_df function (acting similarly to acts_to_lvl but running through DataFrame and accounting for levels) and beginnings of plan_lvl function (commented out currently; will show "path" to level including multiple methods).

--- calc_functions.py
+++ calc_functions.py
@@ -7,6 +7,8 @@ Created on Mon Aug  6 11:23:19 2018
 
 import math
 import csv
+import numpy as np
+import pandas as pd
 
 def add_skill(skill, filepath):
     reader = csv.reader(open(filepath, 'r'))
@@ -25,3 +27,48 @@ def acts_to_lvl(skill, target, current = 1, exp = False) :
         round_val = math.ceil((exp_table[target]-curr_exp)/j)
         exp_dict[i] = round_val
     return exp_dict
+
+
+def acts_lvl_df(skill, target, current = 1, exp = False, showall = False) :
+    """ Generalized skill calculator using pandas DataFrame """
+    if exp == False :
+        curr_exp = exp_table[current] # Returns exp total given level
+    else :
+        curr_exp = current
+    
+    exp_dict = {}
+    for i, row in actions_df[actions_df['skill']==skill].iterrows() :
+        if showall == False and np.logical_and(row['level'] < current, row['always'] != True) :
+            next # Skip underleveled methods
+        elif row['level'] <= target :
+            round_val = math.ceil((exp_table[target]-curr_exp)/row['exp'])
+            exp_dict[row['method']] = round_val # Add relevant methods
+        else:
+            next # Skip overleveled methods
+    return exp_dict
+
+#def plan_lvl(skill, target, current = 1, exp = False, showall = False) :
+#    """ Generalized planner """
+#    def acts_lvl_df(skill, target, current = 1, exp = False, showall = False) :
+#        """ Generalized skill calculator using pandas DataFrame """
+#        if exp == False :
+#            curr_exp = exp_table[current] # Returns exp total given level
+#        else :
+#            curr_exp = current
+#            
+#        exp_dict = {} # Number of actions to level
+#        bw_dict = {} # Methods and levels of "between" methods
+#        for i, row in actions_df[actions_df['skill']==skill].iterrows() :
+#            if showall == False and np.logical_and(row['level'] < current, row['always'] != True) :
+#                next # Skip underleveled methods
+#            elif np.logical_and(row['level'] <= target, row['level'] > current) :
+#                level = row['level']
+#                round_val = math.ceil((exp_table[target]-exp_table[level])/row['exp'])
+#                exp_dict[row['method']] = round_val # Add methods higher than current but less than target
+#                bw_dict[row['method']] = row['level']
+#            elif row['level'] <= target :
+#                round_val = math.ceil((exp_table[target]-curr_exp)/row['exp'])
+#                exp_dict[row['method']] = round_val # Add relevant methods
+#            else:
+#                next # Skip overleveled methods
+#        return exp_dict, bw_dict
